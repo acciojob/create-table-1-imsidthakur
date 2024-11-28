7f213fdae46d85a62ed02011d7feb8d70448e5ec
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-11-28 20:08:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function insert_Row() {
   const table = document.getElementById("sampleTable");
   const tr = document.createElement("tr");
   const td1 = document.createElement("td");
-  td1.textContent = "New Cell1";
+  td1.innerHTML = "New Cell1";
   const td2 = document.createElement("td");
-  td2.textContent = "New Cell2";
+  td2.innerHTML = "New Cell2";
   tr.append(td1,td2);
   table.prepend(tr);
   
